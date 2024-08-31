8f727df788b101e9c76942403103d899cfbfe7b0
shivamalk-23 <k.shivamalk.22@gmail.com>
2024-08-31 13:30:52 +0530

    new addd 31/8.2023

---
+++ hello.java
@@ -0,0 +1,23 @@
+class hello{
+    public static void main(String a[]){
+        int v =22;
+        String stri="  hello";
+        int s=22;
+        s+=22;
+        boolean result= s==v;
+        System.out.println(result);
+        if (s>10)
+             System.out.println("jjjj");  
+        else if (s<22)
+             System.out.println("sssa");
+        else
+            System.out.println("ssadasdasdas"); 
+        
+       int i=0;  
+       while(i<=4){
+        System.out.println("HI");
+        i++;
+        }
+        System.out.print("End");
+    }
+}
